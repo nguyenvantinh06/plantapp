ae6edece82cb3ab18ebbc77b4a791c4a53673d56
Nguyễn Văn Tĩnh <nguyenvantinh06@gmail.com>
2022-02-18 10:24:03 +0700

    + Update Project Setup (Assets, Constants, Screens)

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, DefaultTheme }  from '@react-navigation/native';
+import {Home} from "./src/screens";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+}
+
+export default () => {
+  return <App />
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +29,25 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// import { View, Text, StyleSheet } from 'react-native';
+// import React from 'react';
+
+// const App = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Home</Text>
+//     </View>
+//   )
+// }
+
+// export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
